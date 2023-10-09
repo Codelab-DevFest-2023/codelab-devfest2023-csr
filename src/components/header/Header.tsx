4b8cc4613d714637615480ab062ba396b2a67e7d
Ryan BALOJI <rbaloji@asi.fr>
2023-10-09 10:00:47 +0200

    chore: update header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,17 +7,12 @@ const Header = () => {
       <div className="flex items-center bg-primary lg:mx-44 lg:justify-between lg:py-5  lg:gap-0 gap-6 mx-4 py-2">
         <a href="/" className="flex gap-4 items-center -m-1.5">
           <img src={popcornLogo} alt="Logo popcorn" width={40} height={40} />
-          <span className="hidden sm:block text-white font-semibold leading-6 xl:text-lg text-base">
-            Rendu front, action !
-          </span>
+          <span className="header-title">Rendu front, action !</span>
         </a>
 
-        <ul className="text-white grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+        <ul className="header-item">
           <li>
-            <Link
-              to="/movies"
-              className="flex items-center gap-2 font-semibold leading-6 xl:text-lg text-base"
-            >
+            <Link to="/movies" className="header-link">
               Liste des films
             </Link>
           </li>
